4deceb7965fd4932e60edb382540bf37de7dbe50
sarahzhou6 <48416566+sarahzhou6@users.noreply.github.com>
2019-04-13 13:13:50 -0400

    .

---
+++ Lesson3/lesson.java
@@ -0,0 +1,39 @@
+//for loop
+//used when you know how many times you want to run the program
+//while loop
+//undetermined how many times you want to run the program
+
+/*
+public class file {
+  public static main (String [] args){
+    int i = -2
+    //only recognized within the immediate function
+    for (int j = 0; j <= 100; j++){
+      //only place where the fuction recognizes j
+      //index = j
+      //reference index
+    }
+  }
+  public static void (){
+    //int i = -2 not recognized here
+  }
+}
+*/
+/*
+public class Name /*name should be uppercase*/
+  // getDivisor, getTotalAverage /* lowercase followed by uppercase */
+  // should always start with a verb
+  // tempnum or tmp_num or tmpNum //NEVER Tmp_Num
+
+public static int returnFactorial (int n){
+  if (n == 1){
+    return(1);
+    //return n*returnFactorial(n-1);
+    //returnFactorial(5)
+      //5*returnFactorial(4)
+        //4*returnFactorial(3)
+          //3*returnFactorial(2)
+            //2*returnFactorial(1)
+      //EQUALS 120
+  }
+}
