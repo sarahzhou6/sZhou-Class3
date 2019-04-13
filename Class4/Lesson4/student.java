dbcb0d0e875283c8ef1d770dc9500b5db686a5dd
sarahzhou6 <48416566+sarahzhou6@users.noreply.github.com>
2019-04-13 13:18:23 -0400

    .

---
+++ Class4/Lesson4/student.java
@@ -0,0 +1,16 @@
+public class Student{
+  int grade;
+  String name;
+  public Student(int g){
+    grade = g;
+  }
+  public Student(String n){
+    name = n;
+  }
+  public String getName(){
+    return name;
+  }
+  public int getGrade(){
+    return grade;
+  }
+}
