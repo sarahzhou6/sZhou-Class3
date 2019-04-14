a500101324590873999f941ff6fac73964a44d5c
sarahzhou6 <48416566+sarahzhou6@users.noreply.github.com>
2019-04-14 17:07:39 -0400

    .

---
+++ Class4/Lesson5/Encapsulation/lesson.java
@@ -0,0 +1,11 @@
+//restricts direct access to data of a class
+//fields are set to private
+//each field has a getterand setter method
+
+
+public class lesson {
+  public static void main (String[] args){
+    Employee p1 = new Employee("Josh");
+    System.out.println(p1.name);
+  }
+}
