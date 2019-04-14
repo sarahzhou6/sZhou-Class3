73f3ab81e8160b6db73042badd638bab135db817
sarahzhou6 <48416566+sarahzhou6@users.noreply.github.com>
2019-04-14 17:03:47 -0400

    .

---
+++ Class4/Lesson5/Abstraction/Interface/Audi.java
@@ -0,0 +1,14 @@
+public class Audi implements ParentCar {
+  public static void main(String[] args);
+  int speed = 0;
+  int gear = 1;
+  public void changeGear(int u){
+    gear = u;
+  }
+  public void speedUp(int increment){
+    speed = increment;
+  }
+  public void applyBrakes(int decrement){
+
+  }
+}
