9d5795544fb63caa49edaa26aabb7f228c5cfc31
sarahzhou6 <48416566+sarahzhou6@users.noreply.github.com>
2019-04-14 17:06:28 -0400

    .

---
+++ Class4/Lesson5/Abstraction/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+  public static void main (String[] args){
+    /*Bird b = new Bird();
+    b.label();
+    b.move();
+    b.eat(); */
+    Fish f = new Fish();
+    f.label();
+    f.move();
+    f.eat();
+  }
+}
