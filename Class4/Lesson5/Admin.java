c2109fedd71b3922d12590c5bd25305997e9515a
sarahzhou6 <48416566+sarahzhou6@users.noreply.github.com>
2019-05-03 19:26:43 -0400

    .

---
+++ Class4/Lesson5/Admin.java
@@ -0,0 +1,10 @@
+public class Admin extends User{
+		String name;
+		String email;
+		public Admin(String name,String email){
+				this.name=name;
+				this.email=email;
+		}
+		public void updateProducts(){
+		}
+}
